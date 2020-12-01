49bed1d08f67cefe58d77a0876f169703d3cf279
Rajabov Bobokhon <rboboxon6055@gmail.com>
2020-12-01 23:40:54 +0900

    correction of codes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const searchBtn = document.getElementById("searchBtn");
 const myWord = document.getElementById("myword");
 
+//add event listener to remove box
 document.querySelector("body").addEventListener("click", removeBox);
 
 const baseURL = "https://api.dictionaryapi.dev/api/v2/entries/en";
@@ -28,7 +29,7 @@ function findWord() {
   getTranslation(word)
     .then((response) => {
       if (response.status !== 200) {
-        // alert("Not Found, please write again");
+        //add error message
         document.querySelector("#output").innerHTML = `
         <article class="message is-danger">
         <div class="message-body">Not Found, please try again</div></article>
@@ -40,48 +41,34 @@ function findWord() {
     })
     .then((translation) => {
       console.log({ translation });
-      let output = `<article class="message is-primary">
-      <div class="message-header">
-        <p>Word Info</p>
-        <button class="delete"></button>
-      </div>
+      //upodate html elements
+      let output = `
+      <article class="message is-primary">
+        <div class="message-header">
+          <p>Word Info</p>
+          <button class="delete"></button>
+        </div>
       <div class="message-body">
-        <ul>
-          <li><strong>word: </strong>${translation[0].word}</li>
-          <li><strong>Definition: </strong>${translation[0].meanings[0].definitions[0].definition}</li>
-          <li><strong>Example: </strong>${translation[0].meanings[0].definitions[0].example}</li>
-         
-        </ul>
+          <ul>
+            <li><strong>word: </strong>${translation[0].word}</li>
+            <li><strong>Definition: </strong>${translation[0].meanings[0].definitions[0].definition}</li>
+            <li><strong>Example: </strong>${translation[0].meanings[0].definitions[0].example}</li>
+          </ul>
 
-        <div>
-        <h6 class="audiotxt"><strong>Audio:</strong></h6>
-         <audio controls class="audio  mt-2">
-            <source src="${translation[0].phonetics[0].audio}">
-        </audio>
+          <div>
+              <h6 class="audiotxt"><strong>Audio:</strong></h6>
+              <audio controls class="audio  mt-2">
+                  <source src="${translation[0].phonetics[0].audio}">
+              </audio>
           </div>
-      </div>
+    </div>
     </article>`;
 
-      // let defresult = ` <article class= "message is-"><div class="message-body">
-
-      //       <audio controls class="embed-responsive-item">
-      //       <source src="${translation[0].phonetics[0].audio}">
-      //   </audio>
-      //     </div></article>`;
-
       document.querySelector("#output").innerHTML = output;
-      // document.querySelector("#defresult").innerHTML = defresult;
     });
 
   // e.preventDefault();
 }
-
-// fix undefined
-// function fixUndefined(){
-//   if({translation[0].meanings[0].definitions[0].example} === undefined){
-
-//   }
-// }
 
 //remove word info box
 function removeBox(e) {
