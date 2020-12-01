89647922ec7c1b07d932ba34dc211d1fce8b924c
Rajabov Bobokhon <rboboxon6055@gmail.com>
2020-12-01 23:27:58 +0900

    update audio/design

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ const getTranslation = async (word) => {
   }
 };
 
-function findWord(e) {
+//add enter key
+myWord.addEventListener("keydown", (e) => {
+  e.key === "Enter" && findWord();
+});
+
+function findWord() {
   const word = myWord.value;
 
   //make request using with fetch
@@ -43,18 +48,40 @@ function findWord(e) {
       <div class="message-body">
         <ul>
           <li><strong>word: </strong>${translation[0].word}</li>
-          <li><strong>Defintion: </strong>${translation[0].meanings[0].definitions[0].definition}</li>
+          <li><strong>Definition: </strong>${translation[0].meanings[0].definitions[0].definition}</li>
           <li><strong>Example: </strong>${translation[0].meanings[0].definitions[0].example}</li>
          
         </ul>
+
+        <div>
+        <h6 class="audiotxt"><strong>Audio:</strong></h6>
+         <audio controls class="audio  mt-2">
+            <source src="${translation[0].phonetics[0].audio}">
+        </audio>
+          </div>
       </div>
     </article>`;
 
+      // let defresult = ` <article class= "message is-"><div class="message-body">
+
+      //       <audio controls class="embed-responsive-item">
+      //       <source src="${translation[0].phonetics[0].audio}">
+      //   </audio>
+      //     </div></article>`;
+
       document.querySelector("#output").innerHTML = output;
+      // document.querySelector("#defresult").innerHTML = defresult;
     });
 
-  e.preventDefault();
+  // e.preventDefault();
 }
+
+// fix undefined
+// function fixUndefined(){
+//   if({translation[0].meanings[0].definitions[0].example} === undefined){
+
+//   }
+// }
 
 //remove word info box
 function removeBox(e) {
